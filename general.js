f517af64598b1e2f7f45327f3699926238a716a7
Matheus de Oliveira Abreu <mabreu@pdmfc.com>
2019-01-16 14:33:10 +0000

    feat: getters and setters of objects

---
+++ general.js
@@ -0,0 +1,32 @@
+/**
+ * Getter and Setter
+ */
+
+// Function inside an object
+console.log('Function inside an object');
+console.log('Hard Way');
+const person = {
+    firstName: 'Matheus',
+    lastName: 'Abreu',
+    fullName() {
+        return `${person.firstName} ${person.lastName}`;
+    }
+};
+console.log(person.fullName());
+
+console.log('Smart Way');
+const profile = {
+    firstName: 'Matheus',
+    lastName: 'Abreu',
+    get fullName() {
+        return `${profile.firstName} ${profile.lastName}`;
+    },
+    set fullName(value) {
+        const partsName = value.split(' ');
+        this.firstName = partsName[0];
+        this.lastName = partsName[1];
+    }
+};
+console.log(profile.fullName);
+profile.fullName = 'Matt Abreu';
+console.log(profile.fullName);
